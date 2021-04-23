7976724c1120b4d630f3afd10faeffa3163ee45c
Drew Keat <thedrewkeat@gmail.com>
2021-04-23 12:54:40 -0600

    commit tree 50067e3467fc7efb2a700fd592e0c81ff8342904

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3, :kind
+  
+  KINDS = {
+    equilateral: 1,
+    isosceles: 2,
+    scalene: 3
+  }
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def sides
+    [@s1, @s2, @s3]
+  end
+
+  def equal_sides
+    sides.uniq.length
+  end
+#will return the number of equal sides
+  def kind
+    KINDS.key(equal_sides)
+  end
+#will return: equilateral, if equal_sides == 3; isoceles, if equal_sides == 2; scalene, if equal_sides == 0
 end
+binding.pry
